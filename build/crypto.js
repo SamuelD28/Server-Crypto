0e57a282db8edaf27d4529626f5fa1ba0647ab42
samueldube <samuel_personnel@outlook.com>
2019-03-28 16:01:56 -0400

    RSA keys used to sign message

--- build/crypto.js
+++ build/crypto.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const crypto = require("crypto");
+const keypair = require("keypair");
 class Block {
     constructor(stringToHash) {
         this.Id = Math.trunc(Math.random() * 10);
@@ -9,11 +10,13 @@ class Block {
         this.PreviousHash = "";
         this.Data = new Array();
         this.Date = new Date();
-        this.GeneretateHashProof(stringToHash);
+        this.GeneretateHashProof(stringToHash, false);
     }
-    GeneretateHashProof(input) {
+    GeneretateHashProof(input, verbose) {
         for (let count = 0;; count++) {
             input = crypto.createHash("sha256").update(input).digest("hex");
+            if (verbose)
+                console.log(input);
             if (input.substring(0, 5) === "00000") {
                 this.NbPreuve = count;
                 this.HashProof = input;
@@ -34,7 +37,28 @@ class Block {
             "\nHashProof : " + this.HashProof;
     }
 }
-let block = new Block("");
-console.warn(block.ToString());
-let block2 = new Block("my little pony");
-console.warn(block2.ToString());
+class Cryptography {
+    constructor() {
+        this.LastSignature = new Buffer(2048);
+        let keys = new keypair();
+        this.PublicKey = keys.public;
+        this.PrivateKey = keys.private;
+    }
+    CreateSignature(message) {
+        const signer = crypto.createSign('sha256');
+        signer.update(message);
+        signer.end();
+        this.LastSignature = signer.sign(this.PrivateKey);
+        return this.LastSignature;
+    }
+    VerifySignature(message, signature) {
+        const verifier = crypto.createVerify('sha256');
+        verifier.update(message);
+        verifier.end();
+        return verifier.verify(this.PublicKey, signature);
+    }
+}
+let test = new Cryptography();
+let signature = test.CreateSignature("Allo");
+console.log(test.LastSignature.toString("hex"));
+console.log(test.VerifySignature("Allo", signature));
--- dev/crypto.ts
+++ dev/crypto.ts
@@ -1,4 +1,6 @@
 import crypto = require("crypto");
+import fs = require("fs");
+import keypair = require("keypair");
 
 class Block {
 
@@ -10,12 +12,16 @@ class Block {
     public Date: Date = new Date();
 
     constructor(stringToHash: string) {
-        this.GeneretateHashProof(stringToHash);
+        this.GeneretateHashProof(stringToHash, false);
     }
 
-    GeneretateHashProof(input: string): void {
+    GeneretateHashProof(input: string, verbose: boolean): void {
         for (let count: number = 0; ; count++) {
             input = crypto.createHash("sha256").update(input).digest("hex");
+
+            if (verbose)
+                console.log(input);
+
             if (input.substring(0, 5) === "00000") {
                 this.NbPreuve = count;
                 this.HashProof = input;
@@ -39,7 +45,35 @@ class Block {
     }
 }
 
-let block = new Block("");
-console.warn(block.ToString());
-let block2 = new Block("my little pony");
-console.warn(block2.ToString());
+class Cryptography {
+
+    PrivateKey: string;
+    PublicKey: string;
+    LastSignature: Buffer = new Buffer(2048);
+
+    constructor() {
+        let keys: keypair = new keypair();
+        this.PublicKey = keys.public;
+        this.PrivateKey = keys.private;
+    }
+
+    CreateSignature(message: string): Buffer {
+        const signer = crypto.createSign('sha256');
+        signer.update(message);
+        signer.end();
+        this.LastSignature = signer.sign(this.PrivateKey);
+        return this.LastSignature;
+    }
+
+    VerifySignature(message: string, signature: Buffer): boolean {
+        const verifier = crypto.createVerify('sha256');
+        verifier.update(message);
+        verifier.end();
+        return verifier.verify(this.PublicKey, signature);
+    }
+}
+
+let test = new Cryptography();
+let signature = test.CreateSignature("Allo");
+console.log(test.LastSignature.toString("hex"));
+console.log(test.VerifySignature("Allo", signature));
